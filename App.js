6948e8e90b52f99b511fe93e0b2577214edc4ba2
Enric Franck <enricfranck@gmail.com>
2021-11-16 16:59:45 +0300

    add bar

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Home from './screens/Home'
 import Setting from './screens/Settings'
-import {NavigationContainer} from '@react-navigation/native'
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import { NavigationContainer } from '@react-navigation/native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-const Tab = createBottomTabNavigator();
+const Tab = createBottomTabNavigator(
+);
 import {
   Text,
   View,
@@ -16,8 +18,25 @@ export default function()
   return(
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name='Home' component={Home}/> 
-        <Tab.Screen name='Setting' component={Setting}/> 
+        <Tab.Screen 
+        name='Home' 
+        component={Home} 
+        
+        options={{
+          tabBarShowLabel: false,
+          tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="home" color={color} size={size} />
+          ) }}
+        />
+        <Tab.Screen 
+        name="Settings" 
+        component={Setting}
+        options={{
+          tabBarShowLabel: false,
+          tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="message-text" color={color} size={size} />
+        ) }}
+       /> 
       </Tab.Navigator>
     </NavigationContainer>
   )
--- android/app/src/main/java/com/gasy2gasy/MyReactNative4/MainApplication.java
+++ android/app/src/main/java/com/gasy2gasy/MyReactNative4/MainApplication.java
@@ -20,6 +20,7 @@ import com.swmansion.reanimated.ReanimatedJSIModulePackage;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.oblador.vectoricons.VectorIconsPackage;
 
 public class MainApplication extends Application implements ReactApplication {
   private final ReactNativeHost mReactNativeHost = new ReactNativeHostWrapper(
@@ -36,6 +37,7 @@ public class MainApplication extends Application implements ReactApplication {
       List<ReactPackage> packages = new PackageList(this).getPackages();
       // Packages that cannot be autolinked yet can be added manually here, for example:
       // packages.add(new MyReactNativePackage());
+
       return packages;
     }
 
